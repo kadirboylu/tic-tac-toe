3228db19fbc79848fd59fe2989fa77363d8435b0
kadirboylu <kadir.boylu@outlook.com>
2022-08-18 21:49:01 +0300

    craco.config.js modified

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components/"),
+      "@components": path.resolve(__dirname, "src/Components/"),
       "@contexts": path.resolve(__dirname, "src/contexts/"),
     },
   },
